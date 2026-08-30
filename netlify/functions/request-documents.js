1e084a1f284483f1b8f12d62f2f06554e3a1e541
judaro26 <131112287+judaro26@users.noreply.github.com>
2026-08-29 18:16:07 -0700

    Update request-documents.js

--- netlify/functions/request-documents.js
+++ netlify/functions/request-documents.js
@@ -47,6 +47,7 @@ function buildEmail({ siteName, firstName, message, requested, formUrl }) {
 }
 
 exports.handler = async (event) => {
+  await require('./_lib/apply-email-config')(); // load any custom email provider override before this function's existing nodemailer code runs
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: JSON.stringify({ error: 'Method not allowed' }) };
   }
